c3ac316435fe496b7bf3aa671614bd16480ee54b
Andreas Teneketzoglou <antekai00@gmail.com>
2018-02-23 19:07:19 +0100

    change header

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,13 +5,14 @@ import Container from '../components/Container/Container'
 import Footer from '../components/Footer/Footer'
 import config from '../../config/SiteConfig'
 import Fsform from '../components/Form/Formspree'
+import Hero from '../components/Hero/Hero'
 
 export default class Contact extends React.Component {
 	render() {
 		return (
 			<div className="container contact-container">
 				<Helmet title={`Contact | ${config.siteTitle}`} />
-				<Header>Contact</Header>
+				<Hero title="Contact" subtitle="Just do it!" />
 				<Container text>
 					<h1>Hi!</h1>
 					<p>
