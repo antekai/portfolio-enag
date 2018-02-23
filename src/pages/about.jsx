98705e1fe571fe39abcaad7911a908666d6daf17
Andreas Teneketzoglou <antekai00@gmail.com>
2018-02-23 19:03:42 +0100

    new about header and boiler content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header/Header'
 import Container from '../components/Container/Container'
 import Footer from '../components/Footer/Footer'
 import config from '../../config/SiteConfig'
-
+import Hero from '../components/Hero/Hero'
 
 
 export default class About extends React.Component {
@@ -12,11 +12,11 @@ export default class About extends React.Component {
 		return (
 			<div className="container about-container">
 				<Helmet title={`About | ${config.siteTitle}`} />
-				<Header>About</Header>
+				<Hero title="About" subtitle="Andreas Teneketzoglou" />
 				<Container text>
-					<h1></h1>
-					<p>
 					
+					<p>
+					Multi-disciplined electrical and computer engineer, purpose-oriented and conscientious self-starter, able to strategize and prioritize effectively to accomplish multiple tasks and stay calm under pressure. Flexible and analytical with a keen eye for details; skilled at synthesizing and editing information to achieve overall objectives. Creative, resourceful and flexible, able to adapt to changing priorities and maintain a positive attitude and strong work ethic.
 					</p>
 				</Container>
 				
